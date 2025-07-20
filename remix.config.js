32cd886fd23a3ba9c5c8000ed01f8a97b17a922b
DanPhala <mrphalad@gmail.com>
2025-07-20 02:28:58 +0200

    another deploy to tets

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: "cloudflare-pages",
-  server: "./app/server/index.js", // or "./app/server/index.ts" if using TypeScript and build outputs JS here
+  server: "./server/index.js", // or "./app/server/index.ts" if using TypeScript and build outputs JS here
   ignoredRouteFiles: ["**/.*"],
 };
